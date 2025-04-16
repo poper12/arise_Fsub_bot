f768ab7afbaa7a02fa9ec235c0f7a86e8dd56758
poper12 <samyakdhengle89@gmail.com>
2025-04-16 08:47:46 +0530

    link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -37,7 +37,7 @@ async def batch(client: Client, message: Message):
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-    await second_message.reply_text(f"<b>Here Is Your Link</b>\n\n{link}", quote=True, reply_markup=reply_markup)
+    await second_message.reply_text(f"<b>Here is your link</b>\n\n<blockquotes>`{link}`</blockquotes>", quote=True, reply_markup=reply_markup)
 
 
 
@@ -59,7 +59,7 @@ async def link_generator(client: Client, message: Message):
     base64_string = await encode(f"get-{msg_id * abs(client.db_channel.id)}")
     link = f"https://t.me/{client.username}?start={base64_string}"
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-    await channel_message.reply_text(f"<b>Here Is Your Link</b>\n\n{link}", quote=True, reply_markup=reply_markup)
+    await channel_message.reply_text(f"<b>Here is your link</b>\n\n<blockquotes>`{link}`</blockquotes>", quote=True, reply_markup=reply_markup)
 
 
 
